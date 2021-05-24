576e0baf7248d948d80b0963082da744b06f159a
znz11 <75023238+znz11@users.noreply.github.com>
2021-05-24 22:11:49 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
 node default {
+   file { '/root/README':
+     ensure => file,
+     content => 'This is readme',
+     owner => 'root',
+   }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
